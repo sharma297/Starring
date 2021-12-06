fe12beb7e4c91b58c283cabee5ccd2dddd253245
Hemant Sharma <sharma.hemant@magicsw.com>
2021-12-06 16:47:19 +0530

    pwp assed

--- Starring/src/PatternStar/Star1.java
+++ Starring/src/PatternStar/Star1.java
@@ -1,5 +1,19 @@
 package PatternStar;
 
 public class Star1 {
+	public static void main(String[] args) {
+		Trip tp = new Trip();
+		
+		
+		
+	}
 
 }
+ class Trip{
+	 
+	 private int a =9;
+	 
+	 public void get() {
+		 
+	 }
+ }
